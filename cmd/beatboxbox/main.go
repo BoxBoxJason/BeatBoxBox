37d0f8a9228814fd4db7138ea143284e383b1434
BoxBoxJason <loickgoupil@yahoo.fr>
2024-04-17 20:54:24 +0100

    Prepare backend filesystem

--- cmd/beatboxbox/main.go
+++ cmd/beatboxbox/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
+	fs := http.FileServer(http.Dir("frontend/dist"))
 	http.Handle("/", fs)
 
 	log.Println("Server Up & Listening at https://localhost:8080")
