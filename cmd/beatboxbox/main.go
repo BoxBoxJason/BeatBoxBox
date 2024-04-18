064dd546d3866d5568c7f0e774ab39d5b53ee1a2
BastienTnc <129407226+BastienTnc@users.noreply.github.com>
2024-04-18 19:50:46 +0200

    J'ai ajouté plein de trucs

--- cmd/beatboxbox/main.go
+++ cmd/beatboxbox/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	db_controller "BeatBoxBox/internal/controller/db"
 	"BeatBoxBox/pkg/logger"
 	"net/http"
 )
@@ -9,9 +10,12 @@ func main() {
 	fs := http.FileServer(http.Dir("./frontend/dist"))
 	http.Handle("/", fs)
 
+	db_controller.CreateDB()
+
 	logger.Info("Server Up & Listening at https://localhost:8080")
 	err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
 	if err != nil {
 		logger.Fatal(err)
 	}
+
 }
--- internal/controller/db/db_controller.go
+++ internal/controller/db/db_controller.go
@@ -1,1 +1,49 @@
 package db_controller
+
+import (
+	"BeatBoxBox/pkg/logger"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Musics struct {
+	id       int     `gorm:"primaryKey;autoIncrement"`
+	titre    string  `gorm:"type:text;not null"`
+	auteur   string  `gorm:"type:text;not null"`
+	album    string  `gorm:"type:text"`
+	genre    string  `gorm:"type:text;not null"`
+	nbecoute int     `gorm:"type:int;not null"`
+	note     float32 `gorm:"type:float32;not null"`
+	nbnotes  int     `gorm:"type:int;not null"`
+	path     string  `gorm:"type:varchar(255);not null"`
+}
+
+type Users struct {
+	pseudo                  string `gorm:"type:varchar(32);not null"`
+	subscribed_playlist_ids string `gorm:"type:varchar;not null"`
+	liked_music_ids         string `gorm:"type:varchar;not null"`
+	hashed_password         string `gorm:"type:varchar(64);not null"`
+	salt                    string `gorm:"type:varchar(16);not null"`
+	id                      int    `gorm:"primaryKey;autoIncrement"`
+}
+
+type Playlists struct {
+	titre     string `gorm:"type:text;not null"`
+	music_ids string `gorm:"type:text"`
+	id        int    `gorm:"primaryKey;autoIncrement"`
+	createur  int    `gorm:"foreignKey;autoIncrement"`
+}
+
+func CreateDB() {
+	// Connect to the database
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		logger.Fatal("failed to connect database")
+	}
+
+	// Auto-migrate the schema
+	db.AutoMigrate(&Users{}, &Musics{}, &Playlists{})
+
+	logger.Info("Tables created successfully")
+}
