0302a225bed5223bce8113207b910749622272ef
BastienTnc <129407226+BastienTnc@users.noreply.github.com>
2024-04-18 23:18:37 +0200

    Za Fonzione

--- internal/controller/db/db_controller.go
+++ internal/controller/db/db_controller.go
@@ -9,7 +9,7 @@ import (
 
 type Musics struct {
 	id         int     `gorm:"primaryKey;autoIncrement"`
-	title      string  `gorm:"type:text;not null"`
+	title      string  `gorm:"type:text;unique;not null"`
 	author     string  `gorm:"type:text;not null"`
 	album      string  `gorm:"type:text"`
 	genre      string  `gorm:"type:text"`
@@ -20,7 +20,7 @@ type Musics struct {
 }
 
 type Users struct {
-	pseudo                  string `gorm:"type:varchar(32);not null"`
+	pseudo                  string `gorm:"type:varchar(32);unique;not null"`
 	subscribed_playlist_ids string `gorm:"type:varchar;not null"`
 	liked_music_ids         string `gorm:"type:varchar;not null"`
 	hashed_password         string `gorm:"type:varchar(64);not null"`
@@ -29,21 +29,18 @@ type Users struct {
 }
 
 type Playlists struct {
-	title     string `gorm:"type:text;not null"`
+	title     string `gorm:"type:text;unique;not null"`
 	music_ids string `gorm:"type:text"`
 	id        int    `gorm:"primaryKey;autoIncrement"`
-	creator   int    `gorm:"foreignKey;autoIncrement"`
+	creator   int    `gorm:"foreignKey"`
 }
 
 func CreateDB() {
 	// Connect to the database
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	_, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		logger.Fatal("failed to connect database")
 	}
-
-	// Auto-migrate the schema
-	db.AutoMigrate(&Users{}, &Musics{}, &Playlists{})
 
 	logger.Info("Tables created successfully")
 }
