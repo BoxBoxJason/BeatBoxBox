c79db6e9fc60294c6d42b971470d0d6fe340afcf
BastienTnc <129407226+BastienTnc@users.noreply.github.com>
2024-04-19 12:29:32 +0200

    db fonctionnelle

--- internal/controller/db/db_controller.go
+++ internal/controller/db/db_controller.go
@@ -8,39 +8,44 @@ import (
 )
 
 type Musics struct {
-	id         int     `gorm:"primaryKey;autoIncrement"`
-	title      string  `gorm:"type:text;unique;not null"`
-	author     string  `gorm:"type:text;not null"`
-	album      string  `gorm:"type:text"`
-	genre      string  `gorm:"type:text"`
-	nblistened int     `gorm:"type:int;default 0"`
-	rating     float32 `gorm:"type:float32;default 0"`
-	nbrating   int     `gorm:"type:int;default 0"`
-	path       string  `gorm:"type:varchar(255);not null"`
-}
-
-type Users struct {
-	pseudo                  string `gorm:"type:varchar(32);unique;not null"`
-	subscribed_playlist_ids string `gorm:"type:varchar;not null"`
-	liked_music_ids         string `gorm:"type:varchar;not null"`
-	hashed_password         string `gorm:"type:varchar(64);not null"`
-	salt                    string `gorm:"type:varchar(16);not null"`
-	id                      int    `gorm:"primaryKey;autoIncrement"`
+	Id         int     `gorm:"primaryKey;autoIncrement"`
+	Title      string  `gorm:"type:text;unique;not null"`
+	Author     string  `gorm:"type:text;not null"`
+	Album      string  `gorm:"type:text"`
+	Genre      string  `gorm:"type:text"`
+	Nblistened int     `gorm:"type:int;default 0"`
+	Rating     float32 `gorm:"type:float32;default 0"`
+	Nbrating   int     `gorm:"type:int;default 0"`
+	Path       string  `gorm:"type:varchar(255);not null"`
 }
 
 type Playlists struct {
-	title     string `gorm:"type:text;unique;not null"`
-	music_ids string `gorm:"type:text"`
-	id        int    `gorm:"primaryKey;autoIncrement"`
-	creator   int    `gorm:"foreignKey"`
+	Title     string `gorm:"type:text;unique;not null"`
+	Music_ids string `gorm:"type:text"`
+	Id        int    `gorm:"primaryKey;autoIncrement"`
+	Creator   int    `gorm:"foreignKey"`
 }
 
 func CreateDB() {
+	type Users struct {
+		Pseudo                  string `gorm:"type:varchar(32);unique;not null"`
+		Subscribed_playlist_ids string `gorm:"type:varchar;not null"`
+		Liked_music_ids         string `gorm:"type:varchar;not null"`
+		Hashed_password         string `gorm:"type:varchar(64);not null"`
+		Salt                    string `gorm:"type:varchar(16);not null"`
+		Id                      int    `gorm:"primaryKey;autoIncrement"`
+	}
 	// Connect to the database
-	_, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		logger.Fatal("failed to connect database")
 	}
+
+	db.AutoMigrate(&Users{})
+
+	user := Users{Pseudo: "bob", Subscribed_playlist_ids: "", Liked_music_ids: "", Hashed_password: "1234", Salt: "1234", Id: 1}
+
+	db.Create(&user)
 
 	logger.Info("Tables created successfully")
 }
