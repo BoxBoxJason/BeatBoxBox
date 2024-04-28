fd7af4bd147c81c061e5c751bbe7a8e6141f5622
BoxBoxJason <loickgoupil@yahoo.fr>
2024-04-28 22:22:11 +0100

    Frontend architecture refactor

---
+++ frontend/src/components/music/MusicMiniature.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="music-miniature" style="display: flex; justify-content: space-between; align-items: center;">
+        <div>
+            <h1>{{ title }}</h1>
+            <p>{{ artist }} <span v-if="album">- {{ album }}</span></p>
+        </div>
+        <select>
+            <option value="download">Download</option>
+            <option value="add_to_playlist">Add to a playlist</option>
+            <option value="add_to_waitlist">Add to waitlist</option>
+            <span v-if="album_id"><option value="show_album">Show album</option></span>
+            <option value="show_artist">Show artist</option>
+            <option value="share">Share</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MusicMiniature',
+    props: {
+        music_id: Number,
+        title: String,
+        artist: String,
+        album: String,
+        album_id: Number,
+        path: String
+    }
+}
+</script>
---
+++ frontend/src/components/playlist/Playlist.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="playlist">
+        <h1>{{ title }}</h1>
+        <p>{{ likes }} Like<span v-if="likes > 1">s</span></p>
+        <p>{{ description }}</p>
+        <div>
+            <button>Play</button>
+            <button>Shuffle</button>
+            <button>Download</button>
+            <button>Add to a playlist</button>
+            <button>Add to waitlist</button>
+            <button>Share</button>
+        </div>
+
+        <MusicMiniature v-for="music in musics" :music_id="music.id" :title="music.title" :artist="music.artist" :album="music.album" :album_id="music.album_id" :path="music.path" :illustration="music.illustration"/>
+    </div>
+</template>
+
+import MusicMiniature from '@/components/music/MusicMiniature.vue'
+
+<script>
+export default {
+    name: 'Playlist',
+    props: {
+        playlist_id: Number,
+        title: String,
+        likes: Number,
+        illustration: String,
+        description: String,
+        musics: Array
+    }
+    methods: {
+        
+    }
+}
+
+</script>
