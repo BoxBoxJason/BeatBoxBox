22a7cb44679b7c53c33eef60d29735071a98ffb0
bast <bastrq2@gmail.com>
2024-05-05 17:36:37 +0200

    Auth page updated

--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -1,28 +1,51 @@
 <template>
-    <div>
-      <h2>Login</h2>
-      <form @submit.prevent="login">
-        <input v-model="username" type="text" placeholder="Username" required>
-        <input v-model="password" type="password" placeholder="Password" required>
-        <button type="submit">Login</button>
-      </form>
+  <div>
+    <h2>Authentication</h2>
+    <select v-model="authType">
+      <option value="login">Login</option>
+      <option value="register">Register</option>
+    </select>
+    <div v-if="authType === 'login'">
+      <h3>Login</h3>
+      <input type="text" v-model="loginData.username" placeholder="Username or Email">
+      <input type="password" v-model="loginData.password" placeholder="Password">
+      <button @click="login">Login</button>
     </div>
-  </template>
-  
-  <script>
-  
-  export default {
-    data() {
-      return {
+    <div v-else-if="authType === 'register'">
+      <h3>Register</h3>
+      <input type="text" v-model="registerData.username" placeholder="Username">
+      <input type="email" v-model="registerData.email" placeholder="Email">
+      <input type="password" v-model="registerData.password" placeholder="Password">
+      <button @click="register">Register</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      authType: 'login',
+      loginData: {
         username: '',
         password: ''
+      },
+      registerData: {
+        username: '',
+        email: '',
+        password: ''
       }
+    };
+  },
+  methods: {
+    login() {
+      console.log('Login:', this.loginData);
+      // Add login logic here
     },
-    methods: {
+    register() {
+      console.log('Register:', this.registerData);
+      // Add register logic here
     }
   }
-  </script>
-  
-  <style scoped>
-    /* Your styles here */
-  </style>
+};
+</script>
