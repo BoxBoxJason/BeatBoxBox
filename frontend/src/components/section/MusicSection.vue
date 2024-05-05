82fff185332ca8885b8ec39f51e4ba8115cba292
bast <bastrq2@gmail.com>
2024-05-05 16:58:03 +0200

    Style added

---
+++ frontend/src/components/section/MusicSection.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="section">
+      <h2>{{ title }}</h2>
+      <div class="music-list">
+        <MusicMiniature
+          v-for="music in musics"
+          :key="music.id"
+          :title="music.title"
+          :artist="music.artist"
+          :album="music.album"
+          :album_id="music.album_id"
+          :path="music.path"
+        />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import MusicMiniature from '@/components/music/MusicMiniature.vue'
+  
+  export default {
+    name: 'MusicSection',
+    components: {
+      MusicMiniature
+    },
+    props: {
+      title: String,
+      musics: Array
+    }
+  }
+  </script>
+  
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,11 +3,34 @@
     <img alt="Vue logo" src="../assets/images/logo.png">
     <h1>Welcome to BeatBoxBox, a music platform to listen to your favourite songs ! </h1>
   </div>
+  <div>
+    <MusicSection title="Trending Musics" :musics="trendingMusics" />
+    <MusicSection title="Recently Added Musics" :musics="recentlyAddedMusics " />
+    <MusicSection title="Daily Recommendations" :musics="dailyRecommendations" />
+  </div>
 </template>
 
 <script>
+import MusicSection from '@/components/section/MusicSection.vue'
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    MusicSection
+  },
+  data() {
+    return{
+      trendingMusics: [],
+      recentlyAddedMusics: [],
+      dailyRecommendations: []
+    }
+  }
 
 }
 </script>
+
+<style>
+.trendingMusics {
+  background-color: burlywood;
+} 
+</style>
