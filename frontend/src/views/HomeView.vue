a3017e8435ac018ec1695f200577f2f5f67f7c56
bast <bastrq2@gmail.com>
2024-05-05 13:04:19 +0200

    Title added

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/images/logo.png">
+    <h1>Welcome to BeatBoxBox, a music platform to listen to your favourite songs ! </h1>
   </div>
 </template>
 
