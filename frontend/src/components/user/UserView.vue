e2d091383151862d7ea11c5177e9b8e0a9f70cef
bast <bastrq2@gmail.com>
2024-05-08 10:23:55 +0200

    UserView.vue modified

---
+++ frontend/src/components/user/UserView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h2>User Profile</h2>
+
+    <!-- User Statistics Display -->
+    <div>
+      <h3>User Statistics</h3>
+      <!-- Display user statistics here -->
+    </div>
+
+    <!-- User Information Update Section -->
+    <div>
+      <h3>Update User Information</h3>
+      <!-- Form to update user information -->
+      <form @submit.prevent="updateUser">
+        <!-- Input fields for username, password, email, etc. -->
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username">
+        
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email">
+
+        <!-- Add more input fields for other user information -->
+        
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      email: ''
+      // Add more data properties for other user information
+    };
+  },
+  methods: {
+    updateUser() {
+      // Implement logic to update user information
+      console.log('Updating user information...');
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add styles for the user profile page */
+</style>
