269f2c8942efd535137ea768532507234f238c0e
BoxBoxJason <loickgoupil@yahoo.fr>
2024-05-14 18:25:17 +0100

    Small function name update

--- internal/controller/music/post.go
+++ internal/controller/music/post.go
@@ -5,34 +5,15 @@ import (
 	music_model "BeatBoxBox/internal/model/music"
 	file_utils "BeatBoxBox/pkg/utils/fileutils"
 	"mime/multipart"
-	"path/filepath"
 )
 
 // Checks that all fields are valid, and posts the music to the database and saves the file to the server
 // Returns an error if the music already exists or if there was an error saving the file to the server
 // Returns nil if the music was successfully saved
-func PostMusic(title string, artist_id int, genres []string, album_id int, music_file multipart.File, illustration_file multipart.File) (int, error) {
-	// Generate a new file name & save the music file
-	music_file_name, err := file_utils.CreateNonExistingMusicFileName()
+func PostMusic(title string, genres []string, album_id int, music_file multipart.File, illustration_file_name string) (int, error) {
+	music_file_name, err := file_utils.UploadMusicToServer(music_file)
 	if err != nil {
 		return -1, err
-	}
-	err = file_utils.UploadFileToServer(music_file, filepath.Join("data", "musics", music_file_name))
-	if err != nil {
-		return -1, err
-	}
-
-	// Generate a new file name & save the illustration file
-	illustration_file_name := "default.jpg"
-	if illustration_file != nil {
-		illustration_file_name, err = file_utils.CreateNonExistingIllustrationFileName("musics")
-		if err != nil {
-			return -1, err
-		}
-		err = file_utils.UploadFileToServer(illustration_file, filepath.Join("data", "illustrations", "musics", illustration_file_name))
-		if err != nil {
-			return -1, err
-		}
 	}
 
 	// Open the database, create the music, and close the database
@@ -42,5 +23,5 @@ func PostMusic(title string, artist_id int, genres []string, album_id int, music
 	}
 	defer db_model.CloseDB(db)
 
-	return music_model.CreateMusic(db, title, artist_id, genres, album_id, music_file_name, illustration_file_name)
+	return music_model.CreateMusic(db, title, genres, album_id, music_file_name, illustration_file_name)
 }
--- internal/controller/music/put.go
+++ internal/controller/music/put.go
@@ -13,3 +13,30 @@ func UpdateMusic(music_id int, music_map map[string]interface{}) error {
 	defer db_model.CloseDB(db)
 	return music_model.UpdateMusic(db, music_id, music_map)
 }
+
+func UpdateMusicGenres(music_id int, genres []string) error {
+	db, err := db_model.OpenDB()
+	if err != nil {
+		return err
+	}
+	defer db_model.CloseDB(db)
+	return music_model.UpdateMusic(db, music_id, map[string]interface{}{"genres": genres})
+}
+
+func AddArtistsToMusic(music_id int, artists_ids []int) error {
+	db, err := db_model.OpenDB()
+	if err != nil {
+		return err
+	}
+	defer db_model.CloseDB(db)
+	return music_model.AddArtistsToMusic(db, music_id, artists_ids)
+}
+
+func RemoveArtistFromMusic(music_id int, artists_ids []int) error {
+	db, err := db_model.OpenDB()
+	if err != nil {
+		return err
+	}
+	defer db_model.CloseDB(db)
+	return music_model.RemoveArtistsFromMusic(db, music_id, artists_ids)
+}
