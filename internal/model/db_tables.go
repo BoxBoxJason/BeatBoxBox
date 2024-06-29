978f8496a6d81320574c17519bd65bb6ed32354c
BoxBoxJason <loickgoupil@yahoo.fr>
2024-06-29 23:27:47 +0200

    Fix tables

--- internal/model/db_tables.go
+++ internal/model/db_tables.go
@@ -3,6 +3,7 @@ package db_tables
 import (
 	"BeatBoxBox/pkg/db_model"
 	"BeatBoxBox/pkg/logger"
+	"github.com/lib/pq"
 )
 
 // Initialize the database connection and create the tables
@@ -11,8 +12,12 @@ func init() {
 	if err != nil {
 		logger.Critical("Failed to connect database: ", err)
 	} else {
-		db.AutoMigrate(Artist{}, User{}, Album{}, Music{}, Playlist{}, AuthCookie{})
-		logger.Info("Tables created successfully")
+		err = db.AutoMigrate(Artist{}, User{}, Album{}, Music{}, Playlist{}, AuthCookie{})
+		if err != nil {
+			logger.Info("Tables created successfully")
+		} else {
+			logger.Critical("Failed to create tables: ", err)
+		}
 	}
 }
 
@@ -25,6 +30,8 @@ type Playlist struct {
 	CreatorId    int
 	Creator      User `gorm:"foreignKey:CreatorId"`
 	Protected    bool `gorm:"default:true"`
+	CreatedOn    int  `gorm:"autoCreateTime" json:"created_on"`
+	ModifiedOn   int  `gorm:"autoUpdateTime:milli" json:"modified_on"`
 }
 
 type Album struct {
@@ -33,7 +40,9 @@ type Album struct {
 	Description  string   `gorm:"type:text"`
 	Artists      []Artist `gorm:"many2many:album_artists;"`
 	Illustration string   `gorm:"type:text;default:'default.jpg'"`
-	Musics       []Music  `gorm:"many2many:album_musics;"`
+	Music        []Music  `gorm:"foreignKey:AlbumId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	CreatedOn    int      `gorm:"autoCreateTime" json:"created_on"`
+	ModifiedOn   int      `gorm:"autoUpdateTime:milli" json:"modified_on"`
 }
 
 type Artist struct {
@@ -41,7 +50,9 @@ type Artist struct {
 	Pseudo       string  `gorm:"type:varchar(32);unique;not null"`
 	Bio          string  `gorm:"type:text"`
 	Illustration string  `gorm:"type:text;default:'default.jpg'"`
-	Musics       []Music `gorm:"foreignKey:AlbumId;"`
+	Musics       []Music `gorm:"many2many:artist_musics;"`
+	CreatedOn    int     `gorm:"autoCreateTime" json:"created_on"`
+	ModifiedOn   int     `gorm:"autoUpdateTime:milli" json:"modified_on"`
 }
 
 type Music struct {
@@ -49,35 +60,41 @@ type Music struct {
 	Title        string   `gorm:"type:text;not null"`
 	Lyrics       string   `gorm:"type:text"`
 	Artists      []Artist `gorm:"many2many:artist_musics;"`
-	AlbumId      int
-	Album        Album    `gorm:"foreignKey:AlbumId"`
-	Genres       []string `gorm:"type:text"`
-	Nblistened   int      `gorm:"default:0"`
-	Rating       float32  `gorm:"default:0"`
-	Nbrating     int      `gorm:"default:0"`
-	Likes        int      `gorm:"default:0"`
-	Path         string   `gorm:"type:varchar(36);not null"`
-	Illustration string   `gorm:"type:text;default:'default.jpg'"`
-	UploaderId   int
+	AlbumId      *uint
+	Album        Album          `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Genres       pq.StringArray `gorm:"type:text[]"`
+	Nblistened   int            `gorm:"default:0"`
+	Rating       float32        `gorm:"default:0"`
+	Nbrating     int            `gorm:"default:0"`
+	Likes        int            `gorm:"default:0"`
+	Path         string         `gorm:"type:text;not null"`
+	Illustration string         `gorm:"type:text;default:'default.jpg'"`
+	UploaderId   *uint
 	Uploader     User `gorm:"foreignKey:UploaderId"`
+	CreatedOn    int  `gorm:"autoCreateTime" json:"created_on"`
+	ModifiedOn   int  `gorm:"autoUpdateTime:milli" json:"modified_on"`
 }
 
 type User struct {
+	Id                  int        `gorm:"primaryKey;autoIncrement"`
 	Pseudo              string     `gorm:"type:varchar(32);unique;not null"`
 	Email               string     `gorm:"type:varchar(256);unique;not null"`
 	Hashed_password     string     `gorm:"type:varchar(64);not null"`
-	Id                  int        `gorm:"primaryKey;autoIncrement"`
 	Illustration        string     `gorm:"type:text;default:'default.jpg'"`
 	SubscribedPlaylists []Playlist `gorm:"many2many:user_subscribed_playlists;"`
 	Playlists           []Playlist `gorm:"foreignKey:CreatorId"`
 	LikedMusics         []Music    `gorm:"many2many:user_liked_musics;"`
 	UploadedMusics      []Music    `gorm:"foreignKey:UploaderId"`
+	CreatedOn           int        `gorm:"autoCreateTime" json:"created_on"`
+	ModifiedOn          int        `gorm:"autoUpdateTime:milli" json:"modified_on"`
 }
 
 type AuthCookie struct {
 	Id              int    `gorm:"primaryKey;autoIncrement"`
-	HashedAuthToken string `gorm:"type:varchar(60);not null"`
+	HashedAuthToken string `gorm:"type:text;not null"`
 	UserId          int
 	User            User  `gorm:"foreignKey:UserId"`
 	ExpirationDate  int64 `gorm:"not null"`
+	CreatedOn       int   `gorm:"autoCreateTime" json:"created_on"`
+	ModifiedOn      int   `gorm:"autoUpdateTime:milli" json:"modified_on"`
 }
